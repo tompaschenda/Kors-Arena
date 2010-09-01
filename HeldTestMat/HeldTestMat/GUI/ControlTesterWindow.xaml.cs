570bcc2e9806257ac6ee454f85f48cc74daac80c
Tom Paschenda <tom.paschenda@gmail.com>
2010-09-02 00:27:32 +0200

    GUI durchgestylt.

---
+++ HeldTestMat/HeldTestMat/GUI/ControlTesterWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaktionslogik für ControlTesterWindow.xaml
+    /// </summary>
+    public partial class ControlTesterWindow : Window
+    {
+        public ControlTesterWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
